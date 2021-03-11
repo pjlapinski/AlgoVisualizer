f1a5d123ab9a7852206f75eac417e612b1f3e338
pjlapinski <przemyslaw.j.lapinski@gmail.com>
2021-03-11 16:42:06 +0100

    Routing for all pages

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import HomeView from './Views/Home/HomeView';
+import HomeView from './views/home/HomeView';
+import SortingView from './views/algorithms/sorting/SortingView';
+import SearchingView from './views/algorithms/searching/SearchingView';
+import PathfindingView from './views/algorithms/pathfinding/PathfindingView';
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className='vh-100'>
         <Route path='/' exact component={HomeView} />
+        <Route path='/sorting' component={SortingView} />
+        <Route path='/pathfinding' component={PathfindingView} />
+        <Route path='/searching' component={SearchingView} />
       </div>
     </BrowserRouter>
   );
