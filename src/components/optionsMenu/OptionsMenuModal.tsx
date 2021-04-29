9f3dbec7feb142332437b861b5c62b7073110253
pjlapinski <przemyslaw.j.lapinski@gmail.com>
2021-04-29 12:44:54 +0200

    Final push

--- src/components/optionsMenu/OptionsMenuModal.tsx
+++ src/components/optionsMenu/OptionsMenuModal.tsx
@@ -4,7 +4,6 @@ import { useStoreActions, useStoreState } from '../../store';
 const OptionsMenuModal = () => {
   const simulationDelay = useStoreState(state => state.simulationDelay);
   const setSimulationDelay = useStoreActions(state => state.setSimulationDelay);
-  const switchTheme = useStoreActions(state => state.switchTheme);
 
   return (
     <div className='modal' id='options-menu-modal' tabIndex={-1} role='dialog' aria-hidden='true'>
@@ -12,15 +11,11 @@ const OptionsMenuModal = () => {
         <div className='modal-content bg-primary text-white'>
           <div className='modal-header'>
             <h5 className='modal-title' id='exampleModalLabel'>
-              Opcje
+              Zmień długość przerwy pomiędzy krokami algorytmu
             </h5>
           </div>
           <div className='modal-body text-center'>
-            <button className='btn btn-light col' onClick={() => switchTheme()}>
-              Zmień motyw strony
-            </button>
             <div className='form-group mt-4'>
-              <h4>Zmień szybkość animacji</h4>
               <input
                 type='range'
                 className='custom-range'
@@ -29,6 +24,7 @@ const OptionsMenuModal = () => {
                 min={50}
                 max={1000}
               />
+              <h2>{simulationDelay} ms</h2>
             </div>
           </div>
           <div className='modal-footer'>
